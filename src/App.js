f43e14ce50d4d6a0329f8ffb3e9a3bb632767775
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-05-13 17:41:12 +0100

    Add: useFetchJobs container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import usefetchJobs from './useFetchJobs'
+import useFetchJobs from './useFetchJobs';
+import {Container } from 'react-bootstrap'
 
 function App() {
+  const {jobs, loading, error} = useFetchJobs()
+
   return (
     <div className="App">
-      Hello World
+
+      <Container> 
+        {loading && <h1> Loading ...</h1>}
+        {error && <h1> Error. Try Refreshing.</h1>}
+        <h1> {jobs.length}</h1>
+
+      </Container>
     </div>
   );
 }
