8269d37111f583b7f5b1c680fba80f8ecb85014a
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-05-18 13:26:09 +0100

    Add: JobsPagination added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import useFetchJobs from './useFetchJobs';
 import {Container } from 'react-bootstrap';
 import Job from './Job'
+import JobsPagination from './JobsPagination';
 
 function App() {
   const [params, setParams] = useState({})
@@ -11,17 +12,16 @@ function App() {
 
   return (
     <div className="App">
-
       <Container className='app-Container'>
         <h1 className='app-title' >GitHub Jobs</h1>
+        <JobsPagination page={page} setPage={setPage}/>
         {loading && <h1> Loading ...</h1>}
         {error && <h1> Error. Try Refreshing.</h1>}
         {jobs.map(job => {
           console.log(job)
           return <Job key={job.id} job={job}/> 
         } )}
-
-
+        <JobsPagination page={page} setPage={setPage}/>
       </Container>
     </div>
   );
